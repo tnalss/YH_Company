0cc9934b1536edb8d23beb58ecc9c07329987df5
tnalss <indoubt@naver.com>
2022-12-29 15:18:14 +0900

    project init

---
+++ LastProject/app/src/main/java/com/example/lastproject/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lastproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.conn.ApiClient;
+import com.example.conn.CommonMethod;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ApiClient.setBASEURL("http://localhost/middle/");
+
+
+        new CommonMethod().setParams("email","admin").setParams("pw","admin_pw")
+                            .sendGet("login", new CommonMethod.CallbackResult() {
+            @Override
+            public void result(boolean isResult, String data) {
+                if(isResult)
+                  Log.d("로그", "result: "+data);
+            }
+        });
+
+
+    }
+}
