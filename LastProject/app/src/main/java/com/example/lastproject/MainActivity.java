94d8e60851d5474941081a25bbb09549607681dd
tnalss <indoubt@naver.com>
2022-12-30 13:33:35 +0900

    mainActivity

--- LastProject/app/src/main/java/com/example/lastproject/MainActivity.java
+++ LastProject/app/src/main/java/com/example/lastproject/MainActivity.java
@@ -7,27 +7,23 @@ import android.util.Log;
 
 import com.example.conn.ApiClient;
 import com.example.conn.CommonMethod;
+import com.example.lastproject.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-
+        ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
 
+        /*
         ApiClient.setBASEURL("http://localhost/middle/");
+        로그인에서 해주겠지? */
 
-
-        new CommonMethod().setParams("email","admin").setParams("pw","admin_pw")
-                            .sendGet("login", new CommonMethod.CallbackResult() {
-            @Override
-            public void result(boolean isResult, String data) {
-                if(isResult)
-                  Log.d("로그", "result: "+data);
-            }
-        });
 
 
     }
