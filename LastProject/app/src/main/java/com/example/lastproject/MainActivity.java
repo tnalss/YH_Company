baf35d3f7fec3a5ca347c7c6e46f455cb2399eea
최수민 <indoubt@naver.com>
2022-12-30 16:56:44 +0900

    MainActivity

--- LastProject/app/src/main/java/com/example/lastproject/MainActivity.java
+++ LastProject/app/src/main/java/com/example/lastproject/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.lastproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.example.conn.ApiClient;
-import com.example.conn.CommonMethod;
+
 import com.example.lastproject.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding;
+        double waitTime=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +20,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
 
+        binding.btmNav.setOnItemSelectedListener(item -> {
+            if ( item.getItemId()== R.id.btm_item1 ) {
+                //홈
+            }   else if ( item.getItemId() == R.id.btm_item2 ){
+                //일정
+            }   else if ( item.getItemId() == R.id.btm_item2 ){
+                //출퇴근
+            }   else if ( item.getItemId() == R.id.btm_item2 ){
+                //전자결재
+            }
+            return true;
+        });
         /*
         ApiClient.setBASEURL("http://localhost/middle/");
         로그인에서 해주겠지? */
 
+    }
+    public void changeFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+    }
 
-
+    @Override
+    public void onBackPressed() {
+        if(System.currentTimeMillis() - waitTime >=1500 ) {
+            waitTime = System.currentTimeMillis();
+        } else {
+            super.onBackPressed(); // 액티비티 종료
+        }
     }
 }
