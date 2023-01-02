0682b7a0e4b2e98060fbac69f59f9ae69f5e0448
jooyoung <jooyoung.cairo@gmail.com>
2023-01-02 18:03:19 +0900

    1st

--- LastProject/app/src/main/java/com/example/lastproject/OnActivity.java
+++ LastProject/app/src/main/java/com/example/lastproject/OnActivity.java
@@ -1,16 +1,14 @@
-package com.example.lastproject.attend;
+package com.example.lastproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.lastproject.R;
-
-public class AttendActivity extends AppCompatActivity {
+public class OnActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attend);
+        setContentView(R.layout.activity_on);
     }
 }
--- LastProject/app/src/main/java/com/example/lastproject/attend/attend_main.java
+++ LastProject/app/src/main/java/com/example/lastproject/attend/attend_main.java
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.lastproject.R;
 
 public class attend_main extends Fragment {
 
+    Button on, off;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_attend_main, container, false);
     }
+
+
 }
