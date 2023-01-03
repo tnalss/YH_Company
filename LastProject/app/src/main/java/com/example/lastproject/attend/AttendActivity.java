d39d261a6256112f5a83c67b95ca6de24cf75af7
jooyoung <jooyoung.cairo@gmail.com>
2023-01-03 15:27:15 +0900

    230103
    달력그래들추가

--- LastProject/app/src/main/java/com/example/lastproject/attend/AttendActivity.java
+++ LastProject/app/src/main/java/com/example/lastproject/attend/AttendActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lastproject.R;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class AttendActivity extends AppCompatActivity {
 
@@ -12,5 +13,7 @@ public class AttendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attend);
+
+
     }
 }
